2904bdb175008e49809f86b3deb2a787db068d80
Meggie <mmm25eg@gmail.com>
2016-11-07 19:00:01 +0000

    initial commit from end point on friday week 6 study hall

---
+++ geocoding.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+import geocoder
+resp1 = geocoder.google("Saul's Deli, Berkeley, CA")
+
+resp2 = geocoder.google("Taqueria El Buen Sabor, SF, CA")
+
+resp3 = geocoder.google("Cato's Ale House, Oakland")
+
---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+API_RESULTS = {
+    "Arby's":  {"lng":-122.429763, "lat":37.680394},
+    "Hackbright":{"lng":-122.411540, "lat":37.788862}
+}
+
+@app.route("/")
+def show_homepage():
+    """show the index page"""
+
+    return render_template("blank-slate.html")
+
+
+@app.route("/find-lat-long")
+def find_lat_long():
+    # get what the user typed
+    user_val = request.args.get("place_to_eat")
+    # use an api to find lat long
+    something_found = API_RESULTS.get(user_val)
+    if something_found:
+        return jsonify({'result': something_found})    
+    else:
+        return jsonify({'result': 'Your result was not found.'})
+
+@app.route("/profile/<place_name>")
+def show_profile(place_name):
+    latlong = API_RESULTS.get(place_name)
+    if latlong:
+        return render_template("another.html", latlong=latlong)
+    else:
+        print "\n\n\nNOT FOUND!!\n\n\n"
+        return render_template("blank-slate.html")
+    
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+
